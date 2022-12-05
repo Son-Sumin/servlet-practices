756980eb5e2dc30c5de7d467bccaa7ea307ce322
Son-Sumin <goldkitty007@gmail.com>
2022-12-05 16:23:15 +0900

    배포용 수정

--- emaillist02/src/main/java/com/bitacdemy/emaillist/dao/EmaillistDao.java
+++ emaillist02/src/main/java/com/bitacdemy/emaillist/dao/EmaillistDao.java
@@ -104,7 +104,7 @@ public class EmaillistDao {
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			String url = "jdbc:mysql://192.168.0.107:3306/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 		} catch (ClassNotFoundException e) {
--- guestbook02/src/main/java/com/bitacademy/guestbook/dao/GuestbookDao.java
+++ guestbook02/src/main/java/com/bitacademy/guestbook/dao/GuestbookDao.java
@@ -140,7 +140,7 @@ public class GuestbookDao {
 		try {
 			Class.forName("org.mariadb.jdbc.Driver");
 
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?charset=utf8";
+			String url = "jdbc:mysql://192.168.0.107:3306/webdb?charset=utf8";
 			conn = DriverManager.getConnection(url, "webdb", "webdb");
 			
 		} catch (ClassNotFoundException e) {
